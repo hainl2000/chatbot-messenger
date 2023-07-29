2cfae6ca52a11991543a49862b50c9e16966e20a
hainl2000 <haingluong2000@gmail.com>
2023-07-29 18:09:37 +0700

    update first text

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -123,13 +123,16 @@ let handlePostback = (sender_psid, received_postback) => {
   
     // Get the payload for the postback
     let payload = received_postback.payload;
-  
-    // Set the response based on the postback payload
-    if (payload === 'yes') {
-      response = { "text": "Thanks!" }
-    } else if (payload === 'no') {
-      response = { "text": "Oops, try sending another image." }
+    switch (payload) {
+      case "GET_STARTED":
+        response = { "text": "Chào mừng bạn tới hệ thống!" }
+        break;
+      default:
+        console.log("run default switch");
     }
+
+    // Set the response based on the postback payload
+
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
 }
