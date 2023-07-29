012956c7972be58bd21d160a17b4c8cb3a89ca5c
hainl2000 <haingluong2000@gmail.com>
2023-07-29 15:13:11 +0700

    update code

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -27,16 +27,47 @@ let getWebhook = (req, res) => {
 }
 
 let postWebhook = (req, res) => {
+     // Parse the request body from the POST
     let body = req.body;
 
-    console.log(`\u{1F7EA} Received webhook:`);
-    console.dir(body, { depth: null });
-    if (body.object === "page") {
-        res.status(200).send("EVENT_RECEIVED");
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {
+
+        // Iterate over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+
+            // Get the webhook event. entry.messaging is an array, but 
+            // will only ever contain one event, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+        
+        });
+
+        // Return a '200 OK' response to all events
+        res.status(200).send('EVENT_RECEIVED');
 
     } else {
+        // Return a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
     }
+}
+
+// Handles messages events
+let handleMessage = (sender_psid, received_message) => {
+
+}
+
+// Handles messaging_postbacks events
+let handlePostback = (sender_psid, received_postback) => {
+
+}
+
+// Sends response messages via the Send API
+let callSendAPI = (sender_psid, response) => {
+  
 }
 
 
